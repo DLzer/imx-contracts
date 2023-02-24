c38992e439c04cee72f033590078bb3d8ff0af80
DLzer <dillon.lomnitzer@gmail.com>
2023-02-24 12:06:07 -0500

    Add unchecked blocks to Bytes.sol for safe underflow

--- contracts/utils/Bytes.sol
+++ contracts/utils/Bytes.sol
@@ -14,15 +14,19 @@ library Bytes {
         uint256 temp = value;
         uint256 digits;
         while (temp != 0) {
-            digits++;
-            temp /= 10;
+            unchecked {
+                digits++;
+                temp /= 10;
+            }
         }
         bytes memory buffer = new bytes(digits);
         uint256 index = digits - 1;
         temp = value;
         while (temp != 0) {
-            buffer[index--] = bytes1(uint8(48 + (temp % 10)));
-            temp /= 10;
+            unchecked {
+                buffer[index--] = bytes1(uint8(48 + (temp % 10)));
+                temp /= 10;
+            }
         }
         return string(buffer);
     }
